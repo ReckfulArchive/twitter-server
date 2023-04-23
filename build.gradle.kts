5b01f00689c6e38cc64cf5dc51b71a7db84fa088
IgnatBeresnev <ignat.beresnev@jetbrains.com>
2023-04-23 21:37:21 +0200

    Initialize project

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    kotlin("jvm") version "1.8.20"
+    application
+}
+
+group = "org.reckfularchive"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.8.0")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("MainKt")
+}
