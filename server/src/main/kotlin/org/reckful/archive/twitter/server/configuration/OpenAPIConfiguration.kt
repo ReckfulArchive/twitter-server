b862dac8cc56f8e29e635c59106a83e037997003
IgnatBeresnev <ignat.beresnev@jetbrains.com>
2023-05-28 23:14:01 +0200

    Add profiles for configuration properties

--- server/src/main/kotlin/org/reckful/archive/twitter/server/configuration/OpenAPIConfiguration.kt
+++ server/src/main/kotlin/org/reckful/archive/twitter/server/configuration/OpenAPIConfiguration.kt
@@ -2,25 +2,57 @@ package org.reckful.archive.twitter.server.configuration
 
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class OpenAPIConfiguration {
+class OpenAPIConfiguration(
+    private val configurationProperties: OpenApiProperties
+) {
 
     @Bean
     fun openApi(): OpenAPI {
+        val info = configurationProperties.info
         return OpenAPI()
             .info(
                 Info()
-                    .title("Reckful's Twitter Archive")
+                    .title(info.title)
                     .description(
                         """
-                        API endpoints for querying tweets of or related to Byron (Reckful) Bernstein.
+                        ${info.shortDescription}
                         
-                        You can find this project on GitHub: <a href="https://github.com/ReckfulArchive/twitter">github.com/ReckfulArchive/twitter</a>
+                        You can find this project's source code here: ${beautifyHtmlLink(info.sourceCodeLink)}
                     """.trimIndent()
                     )
             )
+            .servers(
+                configurationProperties.serverUrls.map {
+                    Server().url(it)
+                }
+            )
+    }
+
+    private fun beautifyHtmlLink(link: String): String {
+        val linkWithoutNoise = link
+            .removePrefix("https://")
+            .removePrefix("http://")
+            .removePrefix("www.")
+            .removeSuffix("/")
+
+        return "<a href=\"$link\">$linkWithoutNoise</a>"
     }
 }
+
+@ConfigurationProperties(prefix = "openapi")
+data class OpenApiProperties(
+    val info: OpenApiInfo,
+    val serverUrls: List<String> = emptyList()
+)
+
+data class OpenApiInfo(
+    val title: String,
+    val shortDescription: String,
+    val sourceCodeLink: String,
+)
