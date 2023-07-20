10e5ab61116a0864b47c37f2e3ff9860d22976e4
IgnatBeresnev <ignat.beresnev@jetbrains.com>
2023-07-21 00:55:52 +0200

    Get rid of excessive ping request methods

--- server/src/main/kotlin/org/reckful/archive/twitter/server/controller/PingController.kt
+++ server/src/main/kotlin/org/reckful/archive/twitter/server/controller/PingController.kt
@@ -1,12 +1,12 @@
 package org.reckful.archive.twitter.server.controller
 
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class PingController {
 
-    @RequestMapping("/ping")
+    @GetMapping("/ping")
     fun ping(): String {
         return "OK"
     }
