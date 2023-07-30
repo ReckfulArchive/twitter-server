118e8785c1c82afe934c17ba4be45fa03edac51b
IgnatBeresnev <ignat.beresnev@jetbrains.com>
2023-07-30 13:26:24 +0200

    Hide internal controllers from the docs

--- server/src/main/kotlin/org/reckful/archive/twitter/server/controller/IndexController.kt
+++ server/src/main/kotlin/org/reckful/archive/twitter/server/controller/IndexController.kt
@@ -1,8 +1,10 @@
 package org.reckful.archive.twitter.server.controller
 
+import io.swagger.v3.oas.annotations.Hidden
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 
+@Hidden
 @Controller
 class IndexController {
     @RequestMapping("/")
--- server/src/main/kotlin/org/reckful/archive/twitter/server/controller/PingController.kt
+++ server/src/main/kotlin/org/reckful/archive/twitter/server/controller/PingController.kt
@@ -1,8 +1,10 @@
 package org.reckful.archive.twitter.server.controller
 
+import io.swagger.v3.oas.annotations.Hidden
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Hidden
 @RestController
 class PingController {
 
